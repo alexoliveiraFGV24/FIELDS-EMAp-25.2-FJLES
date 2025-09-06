b07fed77ee021357f53e144bb911f99e27c5f88c
Alex Oliveira <iamsralex@gmail.com>
2025-09-06 13:27:30 -0300

    Reconstruindo para iniciar o dashboard

---
+++ src/system.py
@@ -0,0 +1,17 @@
+from utils import DATA_FOLDER
+from utils.probs import previsao_convolucao, previsao_rna_fft
+import streamlit
+import pandas as pd
+import numpy as np
+
+
+
+class UI:
+    def __init__(self):
+        pass
+
+
+
+class GenerateStats:
+    def __init__(self):
+        pass
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+def DATA_FOLDER():
+    return os.path.join(os.getcwd(), 'files', 'data')
+
+def UTILS_FOLDER():
+    return os.path.join(os.getcwd(), 'utils')
